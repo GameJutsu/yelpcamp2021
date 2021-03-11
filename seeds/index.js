7b390b793d6db2beb768b009d46ba072cb5ac76a
Kumar Vaibhav <rishavvaibhav@gmail.com>
2021-03-11 11:39:39 +0530

    Add image and description to campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -29,9 +29,14 @@ const seedDB = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 20) + 10;
 		const camp = new Campground({
 			title: `${randomArrayElement(descriptors)} ${randomArrayElement(places)}`,
-			location: `${cities[random1000].city}, ${cities[random1000].state}`
+			location: `${cities[random1000].city}, ${cities[random1000].state}`,
+			image: 'https://source.unsplash.com/collection/429524',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.laboris nisi ut aliquip ex ea commodo consequat.',
+			price
 		});
 		await camp.save();
 	}
